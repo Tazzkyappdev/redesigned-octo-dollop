aa64d0f18f199354d45b6d1e0814db1d506d0ada
Tazzkydev <dev@tazzky.com>
2025-08-19 20:26:02 -0600

    chore(favicon): usar nombre nuevo /tazzky-favicon.png?v=6 para evadir caché

--- app/head.tsx
+++ app/head.tsx
@@ -1,10 +1,10 @@
 export default function Head() {
   return (
     <>
-      <link rel="icon" href="/icon.png?v=4" />
-      <link rel="apple-touch-icon" href="/apple-icon.png?v=4" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=4" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=4" />
+      <link rel="icon" href="/tazzky-favicon.png?v=6" />
+      <link rel="apple-touch-icon" href="/tazzky-favicon.png?v=6" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=6" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=6" />
     </>
   )
 }
